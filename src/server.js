4e80054c7ab122d57a238736e33c42a784e437ed
Dulshan Chamika <dulshanchamika2@gmail.com>
2026-02-08 17:41:06 +0530

    update

--- src/server.js
+++ src/server.js
@@ -22,10 +22,6 @@ app.use('/movies', movieRoutes);
 app.use('/auth', authRoutes);
 app.use('/watchlist', watchlistRoutes);
 
-app.get('/hello', (req, res) => {
-    res.json({ message: 'Hello, Worlds!' });
-});
-
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
