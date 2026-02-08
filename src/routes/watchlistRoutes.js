f6523690ec726f8a96ec18d1cb70868ec19cfd64
Dulshan Chamika <dulshanchamika2@gmail.com>
2026-02-08 17:26:44 +0530

    Completed the API

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -5,12 +5,14 @@ import {
     updateWatchlistController
 } from "../controllers/watchlistController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { validateRequest } from "../middleware/validateRequest.js";
+import { addToWatchlistSchema } from "../validators/watchlistValidators.js";
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post("/", addToWatchlistController);
+router.post("/", validateRequest(addToWatchlistSchema), addToWatchlistController);
 router.put("/:id", updateWatchlistController);
 router.delete("/:id", removeFromWatchlistController);
 
